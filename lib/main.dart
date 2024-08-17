b54d9455782bb1a6a2d932267f26b72e461cc977
Abdul Nasir <abdulnasir9323@gmail.com>
2024-08-17 16:18:29 -0700

    '

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:attendance_ms/Screens/Auth/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
