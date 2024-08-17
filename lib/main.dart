0877ff5f14c504cbd6e88c45820544102fd02288
Abdul Nasir <abdulnasir9323@gmail.com>
2024-08-17 16:51:44 -0700

    '

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:attendance_ms/Screens/Auth/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyDcv8_swjT-wSI65_KF5VBBEJvJCPvD6SE",
+        appId: "1:203918850128:android:47ed31634b38354ae8f955",
+        messagingSenderId: "203918850128",
+        projectId: "attendance-management-sy-2d003",
+        storageBucket: "attendance-management-sy-2d003.appspot.com"),
+  );
   runApp(const MyApp());
 }
 
