3b4b8cbbccd0c70f5a33de2eff6574be7a8232a8
Abdul Nasir <abdulnasir9323@gmail.com>
2024-08-18 15:41:05 -0700

    add  check role and authentication status

--- lib/Screens/Auth/splash_screen.dart
+++ lib/Screens/Auth/splash_screen.dart
@@ -1,5 +1,9 @@
+import 'package:attendance_ms/Providers/auth_provider.dart';
+import 'package:attendance_ms/Screens/Admin/home_screen.dart';
 import 'package:attendance_ms/Screens/Auth/sign_in_screen.dart';
+import 'package:attendance_ms/Screens/User/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,12 +16,36 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
+    initSplashScreen();
+  }
+
+  Future<void> initSplashScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+    checkStatus();
+  }
+
+  void checkStatus() async {
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+    await authProvider.fetchUserData();
+
+    if (authProvider.isLoggedIn()) {
+      if (authProvider.isAdmin()) {
+        if (mounted) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const AdminHomeScreen()));
+        }
+      } else {
+        if (mounted) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) => const UserHomeScreen()));
+        }
+      }
+    } else {
       if (mounted) {
         Navigator.pushReplacement(context,
             MaterialPageRoute(builder: (context) => const LoginScreen()));
       }
-    });
+    }
   }
 
   @override
