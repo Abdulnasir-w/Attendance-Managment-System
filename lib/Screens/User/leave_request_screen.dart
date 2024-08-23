239786f98ef04548f486104273a6087b9a1181ce
Abdul Nasir <abdulnasir9323@gmail.com>
2024-08-23 15:09:36 -0700

    Added more screen to user

---
+++ lib/Screens/User/leave_request_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LeaveRequestScreen extends StatelessWidget {
+  const LeaveRequestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/Screens/User/mark_attendance_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MarkAttendanceScreen extends StatelessWidget {
+  const MarkAttendanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
