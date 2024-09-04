19ee65add490f7186917925cac1878263dfb59cd
Abdul Nasir <abdulnasir9323@gmail.com>
2024-09-05 03:39:44 +0500

    it generate report working fine

--- lib/Providers/Admin/generate_report_provider.dart
+++ lib/Providers/Admin/generate_report_provider.dart
@@ -1,20 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
-import 'dart:io';
-
-import 'package:path_provider/path_provider.dart';
-import 'package:share_plus/share_plus.dart';
 
 class GenerateReportProvider extends ChangeNotifier {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
-  Future<void> generateReport() async {
+  List<List<dynamic>> rows = [];
+
+  Future<List<List<dynamic>>> generateReport() async {
     try {
       final users = await firestore.collection("users").get();
-      List<List<dynamic>> rows = [];
-      rows.add(['User Id', "Present", "Absent"]);
-
       for (var user in users.docs) {
+        final username = user['name']; // Fetch the username
         final userId = user.id;
         final attendanceRecords = await firestore
             .collection("Attendance")
@@ -24,26 +19,20 @@ class GenerateReportProvider extends ChangeNotifier {
 
         int presents = 0;
         int absents = 0;
-
-        for (var records in attendanceRecords.docs) {
-          if (records['status'] == "Present") {
+        for (var record in attendanceRecords.docs) {
+          if (record['status'] == "Present") {
             presents++;
-          } else if (records['status'] == 'Absent') {
+          } else if (record['status'] == 'Absent') {
             absents++;
           }
-          rows.add([userId, presents, absents]);
         }
-        String csvData = const ListToCsvConverter().convert(rows);
-        final directory = await getApplicationDocumentsDirectory();
-        final path = "${directory.path}/report.csv";
-        final file = File(path);
-        await file.writeAsString(csvData);
-        Share.share(file.path, subject: "Attendance Report");
-        print("path :: $path");
+        rows.add([username, presents, absents]);
         notifyListeners();
       }
     } catch (e) {
       throw e.toString();
     }
+
+    return rows.isNotEmpty ? rows : [];
   }
 }
